e897b5f005b6e0d358e16bde4895e7d8ae3ed4b5
Alexander Matos lopes <amatos-l@c1r18s6.42lisboa.com>
2022-03-02 07:55:26 +0000

    strlcpy

--- mains/ft_strlcpy.c
+++ mains/ft_strlcpy.c
@@ -5,30 +5,16 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: amatos-l <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/02 07:11:56 by amatos-l          #+#    #+#             */
-/*   Updated: 2022/03/02 07:50:43 by amatos-l         ###   ########.fr       */
+/*   Created: 2022/03/02 07:51:07 by amatos-l          #+#    #+#             */
+/*   Updated: 2022/03/02 07:51:45 by amatos-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+int	main()
 {
-	size_t srcsize;
-	size_t i;
-
-	if (!dst || !src)
-		return (0);
-	srcsize = ft_strlen(src);
-	i = 0;
-	if (dstsize != 0)
-	{
-		while (src[i] != '\0' && i < (dstsize - 1))
-		{
-			dst[i] = src[i];
-			i++;
-		}
-		dst[i] = '\0';
-	}
-	return (srcsize);
+	const char src[] = "World!";
+	char dest[] = "";
+	printf("%zu | %s", ft_strlcpy(dest, src, 3), dest);
 }
